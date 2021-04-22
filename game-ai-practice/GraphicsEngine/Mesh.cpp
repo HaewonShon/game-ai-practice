95ab43f2afa5317d8b198a30bdbf802877fa5cd4
Haewon Shon <haewon8896@gmail.com>
2021-04-22 23:41:13 +0900

    mesh interface
    have to implement cpp

---
+++ game-ai-practice/GraphicsEngine/Mesh.cpp
@@ -0,0 +1,21 @@
+#include "Mesh.h"
+
+void Mesh::AddVertex(const vec2& v)
+{
+}
+
+void Mesh::SetColor(const vec3& c)
+{
+}
+	 
+void Mesh::SetTranslation(const vec2& v)
+{
+}
+
+void Mesh::SetScale(const vec2& v)
+{
+}
+
+void Mesh::SetRotation(double radian)
+{
+}
--- game-ai-practice/GraphicsEngine/Mesh.h
+++ game-ai-practice/GraphicsEngine/Mesh.h
@@ -1,13 +1,23 @@
 #pragma once
 
+#include "Vector2.h"
+#include "Vector3.h"
+#include <vector>
+
 class Mesh
 {
 public:
-	void AddVertex();
-	void SetColor();
+	void AddVertex(const vec2& v);
+	void SetColor(const vec3& c);
 
-	void SetTranslation();
-	void SetScale();
-	void SetRotation();
+	void SetTranslation(const vec2& v);
+	void SetScale(const vec2& v);
+	void SetRotation(double radian);
 private:
+	std::vector<vec2> vertices;
+	vec3 color;
+
+	vec2 translation;
+	vec2 scale;
+	double rotation;
 };
